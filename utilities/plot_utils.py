6b9da7036806d53ac72f834e2f537d5baa204c4b
cxb802 <cxb802@alumni.ku.dk>
2026-03-25 10:20:31 +0100

    outcommenting an unused function in plot_utils

--- utilities/plot_utils.py
+++ utilities/plot_utils.py
@@ -122,122 +122,122 @@ import torchaudio as ta
 
 #     return fig, axes
 
-def plot_features(feats: torch.Tensor, transf: torch.nn.Module, specgram_config: dict
-                  , path: str=None, title="Log-mel spectrogram", ax=None, unit="freqs"
-                  , duration_sec: float=None, t_frames_original: int=None, mel_idx_start:int=0
-                  , n_mels_full: int=None):
-    """
-    Plots a spectrogram from transformed acoustic features.
-    feats = Tensor of shape [C, M, T] where C is number of channels (always 1)
-    , M is number of frequency (mel) bins and T is number of time frames.
-    transf = A transformation class or module.
-    """
+# def plot_features(feats: torch.Tensor, transf: torch.nn.Module, specgram_config: dict
+#                   , path: str=None, title="Log-mel spectrogram", ax=None, unit="freqs"
+#                   , duration_sec: float=None, t_frames_original: int=None, mel_idx_start:int=0
+#                   , n_mels_full: int=None):
+#     """
+#     Plots a spectrogram from transformed acoustic features.
+#     feats = Tensor of shape [C, M, T] where C is number of channels (always 1)
+#     , M is number of frequency (mel) bins and T is number of time frames.
+#     transf = A transformation class or module.
+#     """
     
-    if feats.ndim == 2:
-        Z = feats 
-    elif feats.ndim == 3:
-        Z = feats[0] # Select the first channel.
-    elif feats.ndim == 4:
-        Z = feats[0, 0]
-    else:
-        raise ValueError("Feature tensor dimensions are supported up to 4D.")
+#     if feats.ndim == 2:
+#         Z = feats 
+#     elif feats.ndim == 3:
+#         Z = feats[0] # Select the first channel.
+#     elif feats.ndim == 4:
+#         Z = feats[0, 0]
+#     else:
+#         raise ValueError("Feature tensor dimensions are supported up to 4D.")
     
-    # To numpy for plotting.
-    # Z_np = Z.detach().float().cpu().numpy() # [T, F]    
-    # T, F = Z_np.shape
-    Z_np = Z.detach().float().cpu().numpy()   # [M, T]
-    M, T = Z_np.shape   # M = mel bins, T = time frames
+#     # To numpy for plotting.
+#     # Z_np = Z.detach().float().cpu().numpy() # [T, F]    
+#     # T, F = Z_np.shape
+#     Z_np = Z.detach().float().cpu().numpy()   # [M, T]
+#     M, T = Z_np.shape   # M = mel bins, T = time frames
 
-    assert Z.ndim == 2, f"Expected [M, T], got {Z.shape}"
-    assert Z.shape[0] == specgram_config["n_mels"], f"Expected {specgram_config['n_mels']} mel bins, got {Z.shape[0]}"
+#     assert Z.ndim == 2, f"Expected [M, T], got {Z.shape}"
+#     assert Z.shape[0] == specgram_config["n_mels"], f"Expected {specgram_config['n_mels']} mel bins, got {Z.shape[0]}"
 
-    sr = getattr(transf, "effective_sr", specgram_config.get("sample_rate"))
-    hop = getattr(transf, "hop_length", specgram_config.get("hop_length"))
-    f_min    = getattr(transf, "f_min", specgram_config.get("f_min", 0.0))
-    f_max    = getattr(transf, "f_max", specgram_config.get("f_max", sr / 2.0))
+#     sr = getattr(transf, "effective_sr", specgram_config.get("sample_rate"))
+#     hop = getattr(transf, "hop_length", specgram_config.get("hop_length"))
+#     f_min    = getattr(transf, "f_min", specgram_config.get("f_min", 0.0))
+#     f_max    = getattr(transf, "f_max", specgram_config.get("f_max", sr / 2.0))
 
-    if sr is None or hop is None:
-        raise ValueError("Sample rate and hop length must be defined in either transf or specgram_config.")
-    if duration_sec is not None:
-        t_max = float(duration_sec)
-    elif t_frames_original is not None:
-        t_max = (t_frames_original * hop) / sr 
-    else:
-        t_max = (T * hop) / sr     
+#     if sr is None or hop is None:
+#         raise ValueError("Sample rate and hop length must be defined in either transf or specgram_config.")
+#     if duration_sec is not None:
+#         t_max = float(duration_sec)
+#     elif t_frames_original is not None:
+#         t_max = (t_frames_original * hop) / sr 
+#     else:
+#         t_max = (T * hop) / sr     
 
-    # Z_plot = Z_np.T # [F, T]
-    # bounds = [0.0, t_max, 0, F]
-    Z_plot = Z_np  # already [freq, time]
-    bounds = [0.0, t_max, 0, M]
+#     # Z_plot = Z_np.T # [F, T]
+#     # bounds = [0.0, t_max, 0, F]
+#     Z_plot = Z_np  # already [freq, time]
+#     bounds = [0.0, t_max, 0, M]
 
-    if ax is None: 
-        fig, ax = plt.subplots(figsize=(8, 5))
-    else:
-        fig = None 
+#     if ax is None: 
+#         fig, ax = plt.subplots(figsize=(8, 5))
+#     else:
+#         fig = None 
 
-    im = ax.imshow(Z_plot, origin="lower", aspect="auto", extent=bounds)
-    ax.set_xlabel("Time (s)")
+#     im = ax.imshow(Z_plot, origin="lower", aspect="auto", extent=bounds)
+#     ax.set_xlabel("Time (s)")
 
-    # y-axis can be either labels as mel bins or frequencies.
-    is_mel = getattr(transf, "mel_scale", None) is not None
-    yticks = np.linspace(0, M -1, min(M, 6)).astype(int)
-    ax.set_yticks(yticks)
+#     # y-axis can be either labels as mel bins or frequencies.
+#     is_mel = getattr(transf, "mel_scale", None) is not None
+#     yticks = np.linspace(0, M -1, min(M, 6)).astype(int)
+#     ax.set_yticks(yticks)
 
-    if is_mel:
-        yticks = np.linspace(0, M-1, 6).astype(int)
-        ax.set_yticks(yticks)
+#     if is_mel:
+#         yticks = np.linspace(0, M-1, 6).astype(int)
+#         ax.set_yticks(yticks)
 
-        # If mel bins.
-        if unit == "bins": 
-            ax.set_ylabel("Mel bin")
-            ytick_labels = [str(mel_idx_start + i) for i in yticks]
-            ax.set_yticklabels(ytick_labels)
+#         # If mel bins.
+#         if unit == "bins": 
+#             ax.set_ylabel("Mel bin")
+#             ytick_labels = [str(mel_idx_start + i) for i in yticks]
+#             ax.set_yticklabels(ytick_labels)
             
-        # If frequencies.
-        elif unit == "freq": 
-            ax.set_ylabel("Frequency (kHz)") 
-            f_min = specgram_config.get("f_min", 0.0)
-            f_max = specgram_config.get("f_max", sr / 2.0)
+#         # If frequencies.
+#         elif unit == "freq": 
+#             ax.set_ylabel("Frequency (kHz)") 
+#             f_min = specgram_config.get("f_min", 0.0)
+#             f_max = specgram_config.get("f_max", sr / 2.0)
 
-            if n_mels_full is not None and (mel_idx_start > 0 or n_mels_full != M):
-                fb_full = ta.functional.melscale_fbanks(
-                    n_freqs=specgram_config["n_fft"]//2 + 1, f_min=f_min, f_max=f_max, n_mels=n_mels_full
-                    , sample_rate=sr, mel_scale="htk"
-                    )
-                peak_full = fb_full.argmax(dim=0)
-                peak_slice = peak_full[mel_idx_start: mel_idx_start + M]
-                center_freqs = (peak_slice.float() * (sr / specgram_config["n_fft"])).cpu().numpy()
-            else:
-                # Compute the mel filter bank center frequencies and use as y-axis labels.
-                fb = ta.functional.melscale_fbanks(
-                    n_freqs=specgram_config["n_fft"]//2 + 1, f_min=f_min, f_max=f_max, n_mels=n_mels_full
-                    , sample_rate=sr, mel_scale="htk"
-                    )
-                peak_bins = fb.argmax(dim=0)
-                center_freqs = (peak_bins.float() * (sr / specgram_config["n_fft"])).cpu().numpy()
+#             if n_mels_full is not None and (mel_idx_start > 0 or n_mels_full != M):
+#                 fb_full = ta.functional.melscale_fbanks(
+#                     n_freqs=specgram_config["n_fft"]//2 + 1, f_min=f_min, f_max=f_max, n_mels=n_mels_full
+#                     , sample_rate=sr, mel_scale="htk"
+#                     )
+#                 peak_full = fb_full.argmax(dim=0)
+#                 peak_slice = peak_full[mel_idx_start: mel_idx_start + M]
+#                 center_freqs = (peak_slice.float() * (sr / specgram_config["n_fft"])).cpu().numpy()
+#             else:
+#                 # Compute the mel filter bank center frequencies and use as y-axis labels.
+#                 fb = ta.functional.melscale_fbanks(
+#                     n_freqs=specgram_config["n_fft"]//2 + 1, f_min=f_min, f_max=f_max, n_mels=n_mels_full
+#                     , sample_rate=sr, mel_scale="htk"
+#                     )
+#                 peak_bins = fb.argmax(dim=0)
+#                 center_freqs = (peak_bins.float() * (sr / specgram_config["n_fft"])).cpu().numpy()
 
-            ytick_labels = np.round(center_freqs[yticks] / 1000.0, 1)  # in kHz.
-            ax.set_yticklabels(ytick_labels)
+#             ytick_labels = np.round(center_freqs[yticks] / 1000.0, 1)  # in kHz.
+#             ax.set_yticklabels(ytick_labels)
 
-        # Color bar. 
-        cbar_label = "dB re 1 µPa" if hasattr(transf, "to_db") else "Amplitude"
-    # If linear spectrogram.
-    else:
-        freqs = np.linspace(0, sr / 2, M)
-        ax.set_yticklabels((freqs[yticks] / 1000.0).round(1))
-        ax.set_ylabel("Frequency (kHz)")
-        cbar_label = "dB" if hasattr(transf, "to_db") else "Power"
+#         # Color bar. 
+#         cbar_label = "dB re 1 µPa" if hasattr(transf, "to_db") else "Amplitude"
+#     # If linear spectrogram.
+#     else:
+#         freqs = np.linspace(0, sr / 2, M)
+#         ax.set_yticklabels((freqs[yticks] / 1000.0).round(1))
+#         ax.set_ylabel("Frequency (kHz)")
+#         cbar_label = "dB" if hasattr(transf, "to_db") else "Power"
     
-    cbar = plt.colorbar(im, ax=ax, format="%+2.0f")
-    cbar.set_label(cbar_label)
-    ax.set_title(title)
-    plt.tight_layout()
+#     cbar = plt.colorbar(im, ax=ax, format="%+2.0f")
+#     cbar.set_label(cbar_label)
+#     ax.set_title(title)
+#     plt.tight_layout()
 
-    if path is not None:
-        fig_to_save = fig if fig is not None else ax.figure
-        fig_to_save.savefig(path, dpi=300)
+#     if path is not None:
+#         fig_to_save = fig if fig is not None else ax.figure
+#         fig_to_save.savefig(path, dpi=300)
 
-    return (fig if fig is not None else ax.figure), ax
+#     return (fig if fig is not None else ax.figure), ax
 
 
 def plot_pca_projection(X_pca, evr, window_meta, output_path):
